48825047bdc9d266a1af26f9b5a1fb437297649d
Gaurav Kumar <gauravrajpoot452@gmail.com>
2023-07-01 19:34:11 +0530

    removed fraction library to see if the code works withou it

--- src/js/Views/recipeView.js
+++ src/js/Views/recipeView.js
@@ -1,7 +1,7 @@
 import View from "./view";
 // getting the icon from dist folder that's why used 'url:'
 import icons from "url:../../img/icons.svg";
-import {Fraction} from "fractional";
+// import { Fraction } from "fractional";
 /*
     # means private data.
 */
@@ -74,21 +74,7 @@ class RecipeView extends View {
         
                 <div class="recipe__ingredients">
                     <h2 class="heading--2">Recipe ingredients</h2>
-                    <ul class="recipe__ingredient-list">
-                    ${this._data.ingredients.map(ing => {
-            return `
-                                <li class="recipe__ingredient">
-                                    <svg class="recipe__icon">
-                                        <use href="s${icons}#icon-check"></use>
-                                    </svg>
-                                    <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
-                                    <div class="recipe__description">
-                                        <span class="recipe__unit">${ing.unit}</span>
-                                            ${ing.description}
-                                    </div>
-                                </li>`
-        }).join('')}
-                                    </ul>
+
                 </div>
         
                 <div class="recipe__directions">
@@ -122,3 +108,20 @@ class RecipeView extends View {
 */
 
 export default new RecipeView();
+
+
+{/* <ul class="recipe__ingredient-list">
+    ${this._data.ingredients.map(ing => {
+        return `
+            <li class="recipe__ingredient">
+                <svg class="recipe__icon">
+                    <use href="s${icons}#icon-check"></use>
+                </svg>
+                <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+                <div class="recipe__description">
+                    <span class="recipe__unit">${ing.unit}</span>
+                        ${ing.description}
+                </div>
+            </li>`
+    }).join('')}
+</ul> */}
